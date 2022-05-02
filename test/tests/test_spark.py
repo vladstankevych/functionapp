a3b8d00f3d4dc3187b3902414f58816b6860db5f
vladstankevych <stankevych.v@pg.com>
2022-05-02 22:59:08 +0300

    pytest-spark

--- test/tests/test_spark.py
+++ test/tests/test_spark.py
@@ -1,5 +1,8 @@
 """Tests utilities for spark."""
 import logging
+import pyspark
+import pyspark.sql
+import pytest
 from pandas._typing import FilePath
 from pyspark.sql import functions as sf
 from pyspark.sql import SparkSession, Row
@@ -40,7 +43,7 @@ data = [
 ]
 primary_key_columns = ["column1", "column2"]
 
-spark = get_spark_session()
+#spark = get_spark_session()
 #spark = SparkSession.builder.getOrCreate()
 
 #def test_tracking_change_in_two_row():
@@ -50,32 +53,32 @@ spark = get_spark_session()
 #    type(df)
 #    assert "column1" in df.schema.fieldNames()
 
-def test_just_spark():
+def test_just_spark(spark_session):
     """Tests when a single row has changed."""
     #df = spark.range(100).withColumn("example_data1", sf.rand(seed=42) * 3)
-    df = spark.createDataFrame(data)
+    df = spark_session.createDataFrame(data)
 
     type(df)
     assert "column1" in df.schema.fieldNames()
 
-def test_tracking_change_in_three_row():
+def test_tracking_change_in_three_row(spark_session):
     """Tests when a single row has changed."""
     
-    df = spark.range(100).withColumn("example_data1", sf.rand(seed=42) * 3)
+    df = spark_session.range(100).withColumn("example_data1", sf.rand(seed=42) * 3)
 
     df.write.mode("overwrite").format("delta").saveAsTable("example_table1")
 
-    new_df = spark.table("example_table1")
+    new_df = spark_session.table("example_table1")
     assert "example_data1" in new_df.schema.fieldNames()
 
-def test_tracking_change_in_four_row():
+def test_tracking_change_in_four_row(spark_session):
     """Tests when a single row has changed."""
     
-    df = spark.range(100).withColumn("example_data2", sf.rand(seed=42) * 3)
+    df = spark_session.range(100).withColumn("example_data2", sf.rand(seed=42) * 3)
 
     df.write.mode("overwrite").format("delta").saveAsTable("example_table2")
 
-    new_df = spark.table("example_table2")
+    new_df = spark_session.table("example_table2")
     assert "example_data2" in new_df.schema.fieldNames()
 
 #def test_tracking_change_in_five_row():

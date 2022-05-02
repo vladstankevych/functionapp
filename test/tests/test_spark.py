89cf5179526a1ad757acf95ca666bef0b2de9fea
vladstankevych <stankevych.v@pg.com>
2022-05-02 21:23:18 +0300

    Just spark (2 func)

--- test/tests/test_spark.py
+++ test/tests/test_spark.py
@@ -40,48 +40,49 @@ data = [
 ]
 primary_key_columns = ["column1", "column2"]
 
-spark = get_spark_session()
+#spark = get_spark_session()
+spark = SparkSession.builder.getOrCreate()
 
-#def test_tracking_change_in_two_row():
+def test_tracking_change_in_two_row():
+    """Tests when a single row has changed."""
+    df = spark.createDataFrame(data)
+
+    type(df)
+    assert "column1" in df.schema.fieldNames()
+
+def test_just_spark():
+    """Tests when a single row has changed."""
+    df = spark.createDataFrame(data)
+
+    type(df)
+    assert "column1" in df.schema.fieldNames()
+
+#def test_tracking_change_in_three_row():
 #    """Tests when a single row has changed."""
-#    #spark = SparkSession.builder.getOrCreate()
-#    spark = get_spark_session()
-#
-#    df = spark.createDataFrame(data)
-#
-#    type(df)
 #    
-#    #df = spark.range(10000000).withColumn("example_data", sf.rand(seed=42) * 3)
-#    #df.write.mode("overwrite").format("delta").saveAsTable("example_table")
-#    #new_df = spark.table("example_table")
-#    assert "column1" in df.schema.fieldNames()
+#    df = spark.range(10000000).withColumn("example_data1", sf.rand(seed=42) * 3)
+#
+#    df.write.mode("overwrite").format("delta").saveAsTable("example_table1")
+#
+#    new_df = spark.table("example_table1")
+#    assert "example_data1" in new_df.schema.fieldNames()
 
-def test_tracking_change_in_three_row():
-    """Tests when a single row has changed."""
-    
-    df = spark.range(10000000).withColumn("example_data1", sf.rand(seed=42) * 3)
+#def test_tracking_change_in_four_row():
+#    """Tests when a single row has changed."""
+#    
+#    df = spark.range(10000000).withColumn("example_data2", sf.rand(seed=42) * 3)
+#
+#    df.write.mode("overwrite").format("delta").saveAsTable("example_table2")
+#
+#    new_df = spark.table("example_table2")
+#    assert "example_data2" in new_df.schema.fieldNames()
 
-    df.write.mode("overwrite").format("delta").saveAsTable("example_table1")
-
-    new_df = spark.table("example_table1")
-    assert "example_data1" in new_df.schema.fieldNames()
-
-def test_tracking_change_in_four_row():
-    """Tests when a single row has changed."""
-    
-    df = spark.range(10000000).withColumn("example_data2", sf.rand(seed=42) * 3)
-
-    df.write.mode("overwrite").format("delta").saveAsTable("example_table2")
-
-    new_df = spark.table("example_table2")
-    assert "example_data2" in new_df.schema.fieldNames()
-
-def test_tracking_change_in_five_row():
-    """Tests when a single row has changed."""
-    
-    df = spark.range(10000000).withColumn("example_data3", sf.rand(seed=42) * 3)
-
-    df.write.mode("overwrite").format("delta").saveAsTable("example_table3")
-
-    new_df = spark.table("example_table3")
-    assert "example_data3" in new_df.schema.fieldNames()
+#def test_tracking_change_in_five_row():
+#    """Tests when a single row has changed."""
+#    
+#    df = spark.range(10000000).withColumn("example_data3", sf.rand(seed=42) * 3)
+#
+#    df.write.mode("overwrite").format("delta").saveAsTable("example_table3")
+#
+#    new_df = spark.table("example_table3")
+#    assert "example_data3" in new_df.schema.fieldNames()

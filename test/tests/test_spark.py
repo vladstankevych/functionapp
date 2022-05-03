26ce767e40f598caa452b87ff1fd81fc0a1228bb
vladstankevych <stankevych.v@pg.com>
2022-05-03 15:56:17 +0300

    Configure spark

--- test/tests/test_spark.py
+++ test/tests/test_spark.py
@@ -55,30 +55,30 @@ spark = get_spark_session()
 #    type(df)
 #    assert "column1" in df.schema.fieldNames()
 
-def test_just_spark(spark_session):
+def test_just_spark():
     """Tests when a single row has changed."""
     #df = spark.range(100).withColumn("example_data1", sf.rand(seed=42) * 3)
-    df = spark_session.createDataFrame(data)
+    df = spark.createDataFrame(data)
 
     type(df)
     assert "column1" in df.schema.fieldNames()
 
 def test_tracking_change_in_three_row():
     """Tests when a single row has changed."""
-    df = spark.range(10000000).withColumn("example_data", sf.rand(seed=42) * 3)
-    df.write.mode("overwrite").format("delta").saveAsTable("example_table")
+    df = spark.range(10000000).withColumn("example_data1", sf.rand(seed=42) * 3)
+    df.write.mode("overwrite").format("delta").saveAsTable("example_table1")
 
     new_df = spark.table("example_table1")
     assert "example_data1" in new_df.schema.fieldNames()
 
-def test_tracking_change_in_four_row(spark_session):
+def test_tracking_change_in_four_row():
     """Tests when a single row has changed."""
     
-    df = spark_session.range(100).withColumn("example_data2", sf.rand(seed=42) * 3)
+    df = spark.range(100).withColumn("example_data2", sf.rand(seed=42) * 3)
 
     df.write.mode("overwrite").format("delta").saveAsTable("example_table2")
 
-    new_df = spark_session.table("example_table2")
+    new_df = spark.table("example_table2")
     assert "example_data2" in new_df.schema.fieldNames()
 
 #def test_tracking_change_in_five_row():

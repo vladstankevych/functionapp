27fb590baa839518f61527c90e2c0ac23aae2a22
vladstankevych <stankevych.v@pg.com>
2022-11-24 13:09:34 +0200

    Check with a better output

--- func/TimerTrigger1/__init__.py
+++ func/TimerTrigger1/__init__.py
@@ -10,6 +10,6 @@ def main(mytimer: func.TimerRequest) -> None:
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
-    print("Cum")
+    print("Let")
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
